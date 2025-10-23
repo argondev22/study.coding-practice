c38c10c204badf67d8ed810c341782779850348e
argondev22 <hyoga.js@gmail.com>
2025-10-23 22:42:39 +0900

    Add in-place merge implementation for "Merge Sorted Array" problem in TypeScript, including sorting logic for combined arrays.

---
+++ src/leetcode/88_merge-sorted-array/20251023.ts
@@ -0,0 +1,19 @@
+/**
+ Do not return anything, modify nums1 in-place instead.
+ */
+ function merge(nums1: number[], m: number, nums2: number[], n: number): void {
+    for (let i = 0; i < n; i++) {
+        nums1[m + i] = nums2[i];
+    }
+
+    for (let i = 0; i < m + n; i++) {
+        for (let j = 0; j < m + n; j++) {
+            const preElement = nums1[j];
+            const postElement = nums1[j + 1];
+            if (preElement > postElement) {
+                nums1[j] = postElement;
+                nums1[j + 1] = preElement;
+            }
+        }
+    }
+};
