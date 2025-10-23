e719c9c033bec84701569e556628508726e7b7b7
argondev22 <hyoga.js@gmail.com>
2025-10-24 07:47:40 +0900

    Add implementation for "Remove Element" problem in TypeScript, utilizing a two-pointer technique to efficiently remove specified values from the array.

---
+++ src/leetcode/27_remove-element/20251024.ts
@@ -0,0 +1,25 @@
+export function removeElement(nums: number[], val: number): number {
+    const removedNum = -1;
+    let i = nums.length - 1;
+    let j = nums.length - 1;
+
+    while (i >= 0) {
+        if (nums[i] === val) {
+            if (i === j) {
+                nums[j] = removedNum;
+            } else {
+                nums[i] = nums[j];
+                nums[j] = removedNum;
+            }
+            j--;
+        }
+        i--;
+    }
+
+    return j + 1;
+};
+
+/**
+ * Status: Accepted
+ * Time taken: 28 m 23 s
+ */
