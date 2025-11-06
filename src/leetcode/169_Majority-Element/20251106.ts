74b1847db514a4486f72ecf4b788e8b8808bb5db
Hyoga <hyoga.js@gmail.com>
2025-11-06 22:03:14 +0900

    Implement majorityElement function to find majority

---
+++ src/leetcode/169_Majority-Element/20251106.ts
@@ -0,0 +1,17 @@
+export function majorityElement(nums: number[]): number {
+    let majority = nums[0];
+    let count = 1;
+
+    for (let i = 1; i < nums.length; i++) {
+        if (count === 0) {
+            majority = nums[i];
+            count = 1;      
+        } else if (nums[i] === majority) {
+            count++
+        } else {
+            count--
+        }
+    }
+
+    return majority;
+};
